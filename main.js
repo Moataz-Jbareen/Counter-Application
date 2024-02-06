3e733d28815bbcf809b1535c831acb97108d1e70
Moataz <moatazw72@gmail.com>
2024-02-06 23:38:17 +0200

    implements the counter  functions (increment decrement and reset the counter

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 document.getElementById("increment").addEventListener("click", incrementcounter);
 document.getElementById("decrement").addEventListener("click", decrementcounter);
 document.getElementById("reset").addEventListener("click", resetcounter);
+
+
+let counterDisplayed = document.getElementById("counter-d");
+
+let counterValue=0;
+
+function updateCounterDisplay(){
+    counterDisplayed.textContent = counterValue;
+
+}
+
+
+
+function incrementcounter(){
+    counterValue++;
+    updateCounterDisplay()
+}
+
+
+
+function decrementcounter(){
+
+    if(counterValue>0){
+        counterValue--;
+        updateCounterDisplay()
+    }
+    
+}
+
+function resetcounter(){
+    counterValue=0;
+    updateCounterDisplay()
+}
