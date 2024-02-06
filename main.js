77aa90b53a02e0471553c9ea6caf87bb8a558fa8
Moataz <moatazw72@gmail.com>
2024-02-06 23:25:01 +0200

    adding three eventlistener functions to handle increment and decrment and reset buttons in js file

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("increment").addEventListener("click", incrementcounter);
+document.getElementById("decrement").addEventListener("click", decrementcounter);
+document.getElementById("reset").addEventListener("click", resetcounter);
